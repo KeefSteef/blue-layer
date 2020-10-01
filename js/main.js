dcb52f9ad284ae144c379b5a72d74034d32d30cf
Keef <deazy01025@gmail.com>
2020-10-01 14:08:12 +0300

    production

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+gsap.to('.scroll',{
+    scrollTrigger:{
+        trigger:'.scroll',
+        toggleActions: "restart"
+    },
+    x:0,
+    duration:.5
+
+})
+
+document.querySelector('.scroll').addEventListener('click',()=>{
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+
+gsap.from('.offers__title h2',{
+    scrollTrigger:{
+        trigger:'.card__info-title'
+    },
+    opacity:0
+})
